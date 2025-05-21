3d441cea253945e9ddb998ceca094678cbeb8a0f
felipemarley <felipemarley34@gmail.com>
2025-05-20 21:44:38 -0300

    route transitions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <Header />
-    <router-view />
+    <Header /> 
+    <transition name="fade" mode="out-in">
+      <router-view />
+    </transition>
     <Footer />
   </div>
 </template>
@@ -10,3 +12,12 @@
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
 </script>
+
+<style>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+.fade-enter-from, .fade-leave-to {
+  opacity: 0;
+}
+</style>
